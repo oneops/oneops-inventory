9f8590b3fc9407033b47f17432c26b1ca88b44d7
Jason Dillon <jason@planet57.com>
2017-09-28 16:37:39 -0700

    simpler test that does the same Rigorous thing

--- src/test/java/com/oneops/AppTest.java
+++ src/test/java/com/oneops/AppTest.java
@@ -1,38 +1,19 @@
 package com.oneops;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple Inventory.
  */
 public class AppTest 
-    extends TestCase
 {
     /**
-     * Create the test case
-     *
-     * @param testName name of the test case
+     * Rigorous Test :-)
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testApp() {
+        assertTrue(true);
     }
 }
